b1a0196b1b1b0699ebe5fa0f0cf45ab2c6f931a5
Aki Chang <wangchang0409@163.com>
2026-05-26 15:19:44 +0800

    debug: migrations

--- drizzle/migrations/0006_confused_vertigo.sql
+++ drizzle/migrations/0006_confused_vertigo.sql
@@ -16,22 +16,4 @@ CREATE TABLE `invite_codes` (
 	FOREIGN KEY (`used_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `invite_codes_code_unique` ON `invite_codes` (`code`);--> statement-breakpoint
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
-CREATE TABLE `__new_users` (
-	`id` text PRIMARY KEY NOT NULL,
-	`email` text NOT NULL,
-	`name` text NOT NULL,
-	`email_verified` integer DEFAULT false NOT NULL,
-	`image` text,
-	`role` text DEFAULT '["owner"]' NOT NULL,
-	`created_at` integer NOT NULL,
-	`updated_at` integer NOT NULL,
-	`deleted_at` text
-);
---> statement-breakpoint
-INSERT INTO `__new_users`("id", "email", "name", "email_verified", "image", "role", "created_at", "updated_at", "deleted_at") SELECT "id", "email", "name", "email_verified", "image", "role", "created_at", "updated_at", "deleted_at" FROM `users`;--> statement-breakpoint
-DROP TABLE `users`;--> statement-breakpoint
-ALTER TABLE `__new_users` RENAME TO `users`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;--> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
+CREATE UNIQUE INDEX `invite_codes_code_unique` ON `invite_codes` (`code`);
